b5d37507834eb847f8745c1ab7e9172931e17ae8
M Hillman <mdhillman@cmclinnovations.com>
2014-10-17 18:10:30 +0100

    develop: added bare-bones PHP project generated by netbeans.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Liopleurodon Test</title>
+    </head>
+    
+    <body>
+        <p>Today's date (according to this server) is...</p>
+        <?php
+            echo(date('l, F dS Y.'));
+        ?>
+    </body>
+</html>
